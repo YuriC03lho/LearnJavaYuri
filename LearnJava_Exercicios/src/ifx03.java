0e20a5f4896e0ac83679513b8ba11fd750670095
YuriC03lho <yuricoelhovieira@gmail.com>
2026-01-05 21:38:56 -0300

    Destruidores de CLT

---
+++ LearnJava_Exercicios/src/ifx03.java
@@ -0,0 +1,21 @@
+import java.lang.Math;
+public class ifx03 {
+public static void main(String[]args) {
+	
+	int _InputValue1,_InputValue2;
+	
+	_InputValue1 = 24;
+	_InputValue2 = 6;
+	
+	
+	if(_InputValue2 % 2 == 0 ) {
+		System.out.println("São multiplos");
+		
+	}
+	else {
+		System.out.println("Não são multiplos");
+	}
+}
+
+//Fiquei com preguiça de por Scanner KKK.
+}
