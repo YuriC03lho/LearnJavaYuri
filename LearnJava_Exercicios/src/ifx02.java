ede5d26e3525b14d4808170a23fdacb31b57e2e4
YuriC03lho <yuricoelhovieira@gmail.com>
2026-01-05 21:21:08 -0300

    Finalizado mais 1

---
+++ LearnJava_Exercicios/src/ifx02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ifx02 {
+public static void main(String[]args) {
+	Scanner sc = new Scanner(System.in);
+	
+	int _InputInt = sc.nextInt();
+	
+	if(_InputInt % 2 == 0)
+	{
+		System.out.printf("%d é par", _InputInt);
+	}
+	else {
+		System.out.printf("%d é Impar", _InputInt);
+	}
+}
+}
+
+// Im Still Standing (Extended) - Taron Egerton - kingsTaron (Youtube) The first match.
